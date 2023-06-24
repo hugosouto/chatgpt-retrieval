c172fbcb11fb8e0f087a86381ad940fda3fb1baf
Hugo Souto <hugomsouto@gmail.com>
2023-06-24 08:13:18 -0300

    Simplified version

--- chatgpt.py
+++ chatgpt.py
@@ -1,41 +1,20 @@
-import os
+import os 
 import sys
 
-# import openai
-from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import OpenAI
-from langchain.vectorstores import Chroma
+from langchain.document_loaders import TextLoader 
+from langchain.document_loaders import DirectoryLoader 
+from langchain.indexes import VectorstoreIndexCreator 
+from langchain.llms import OpenAI 
+from langchain.chat_models import ChatOpenAI 
 
 # Read API keyfrom the environment variable OPENAI_API_KEY
 APIKEY = os.getenv("OPENAI_API_KEY")
 
-# Enable to cache & reuse the model to disk (for repeated queries on the same data)
-PERSIST = False
+# query = sys.argv[1]
+query = "Em quais minitérios foi dividido o Ministério da Economia no governo Lula?"
 
-query = sys.argv[1]
+loader = TextLoader('data.txt') 
+# loader = DirectoryLoader(".", glob="*.txt")  
+index = VectorstoreIndexCreator().from_loaders([loader])
 
-if PERSIST and os.path.exists("persist"):
-    print("Reusing index...\n")
-    vectorstore = Chroma(persist_directory="persist",
-                         embedding_function=OpenAIEmbeddings())
-    from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-    loader = TextLoader('data.txt')
-    # This code can also import folders, including various filetypes like PDFs using the DirectoryLoader.
-    # loader = DirectoryLoader(".", glob="*.txt")
-    if PERSIST:
-        index = VectorstoreIndexCreator(
-            vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
-    else:
-        index = VectorstoreIndexCreator().from_loaders([loader])
-
-chain = RetrievalQA.from_chain_type(
-    llm=ChatOpenAI(model="gpt-3.5-turbo"),
-    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
-print(chain.run(query))
+print(index.query(query, llm=ChatOpenAI(model="gpt-4")))
